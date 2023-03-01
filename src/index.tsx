424f2b1423a667d6bc00908bd9eefa617acc09ce
Vijay Pratap <vijay.singh@hestabit.in>
2023-03-01 13:49:50 +0530

    code pull

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,5 @@
 import SignupForm from './components/SignupForm';
 import LoginForm from './components/LoginForm';
-import GoogleButton from './components/GoogleButton'
-import FacebookButton from './components/FacebookButton'
 import ResetPasswordForm from './components/ResetPasswordForm';
 import ForgotPasswordForm from './components/ForgotPasswordForm';
 import ForgotPasswordVerifyOTPForm from './components/ForgotPasswordVerifyOTPForm';
@@ -13,8 +11,6 @@ export default useProvider;
 export {
 	SignupForm,
 	LoginForm,
-	GoogleButton,
-	FacebookButton,
 	ResetPasswordForm,
 	ForgotPasswordForm,
 	ForgotPasswordVerifyOTPForm,
